fd499b8f3f5a460edeb1103ceac4f7e84306e867
Andreas Fleig <github@andyfleig.de>
2018-01-03 21:22:50 +0100

    implemented set 1 challenge 3

--- set1/challenges.go
+++ set1/challenges.go
@@ -9,10 +9,11 @@ import "fmt"
 import "encoding/base64"
 import "encoding/hex"
 
-// challange 2
+// challange 3
+import "bytes"
 
 func main() {
-
+  singleByteXORCipher("ETAOIN SHRDLU")
 }
 
 // challenge 1
@@ -35,4 +36,55 @@ func fixedXOR(in1 []byte, in2 []byte) []byte {
     result[i] = in1[i] ^ in2[i]
   }
   return result
+}
+
+func singleByteXORCipher(in string) string {
+  in_hex, err := hex.DecodeString(in)
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  in_arr := []byte(in_hex)
+  r := 'a'
+  r = 33
+
+  best_score := 0
+  best_result := make([]byte, len(in_hex))
+  // loop over all characters:
+  for ;r < 127; r++ {
+    // create byte slice with same length as in and all characters are r:
+    arr := make([]byte, len(in_arr))
+    for i := 0; i < len(in_arr); i++ {
+      arr[i] = byte(r)
+    }
+    // XOR the two byte slices
+    result := fixedXOR(arr, in_arr)
+
+    // score the result:
+    score := 0
+    for i := 0; i < len(result); i++ {
+      char := result[i:i+1]
+      if (bytes.Equal(char, []byte("e"))) {score += 127}
+      if (bytes.Equal(char, []byte("t"))) {score += 90}
+      if (bytes.Equal(char, []byte("a"))) {score += 82}
+      if (bytes.Equal(char, []byte("o"))) {score += 75}
+      if (bytes.Equal(char, []byte("i"))) {score += 70}
+      if (bytes.Equal(char, []byte("n"))) {score += 67}
+      if (bytes.Equal(char, []byte("s"))) {score += 63}
+      if (bytes.Equal(char, []byte("h"))) {score += 61}
+      if (bytes.Equal(char, []byte("r"))) {score += 60}
+      if (bytes.Equal(char, []byte("d"))) {score += 43}
+
+      if (bytes.Equal(char, []byte("*"))) {score -= 100}
+    }
+
+    if (score > best_score) {
+      best_result = result
+      best_score = score
+    }
+
+  }
+  return string(best_result[:len(in_arr)])
+  // loop over all characters:
+
 }
--- set1/challenges_test.go
+++ set1/challenges_test.go
@@ -33,3 +33,10 @@ func TestChallenge2(t *testing.T) {
     t.Error("c2: wrong result")
   }
 }
+
+func TestChallenge3(t *testing.T) {
+  res := singleByteXORCipher("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+  if res != "Cooking MC's like a pound of bacon" {
+    t.Error("c3: wrong result", res)
+  }
+}
