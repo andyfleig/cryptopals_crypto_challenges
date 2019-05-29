bc9f924d9baf7c45cbcd79479b5ceea6311c9228
Andreas Fleig <github@andyfleig.de>
2019-05-29 12:05:59 +0200

    implemented set 2 challenge 10

--- set2.go
+++ set2.go
@@ -3,12 +3,63 @@
 
 package main
 
+import "fmt"
+
 // challenge 9
 import "bytes"
 
+// challenge 10
+import "crypto/aes"
+
 // challenge 9
 func addPkcsPadding(in []byte, blocksize int) []byte {
+  if len(in)%blocksize == 0 {
+    return in
+  }
   padding_length := blocksize - (len(in)%blocksize)
   padding := bytes.Repeat([]byte("\x04"), padding_length)
   return append(in, padding...)
+}
+
+func encryptAesEcb(key []byte, plaintext []byte) []byte{
+  keysize := len(key)
+  block, err := aes.NewCipher(key)
+  if (err != nil) {
+    fmt.Println(err)
+  }
+  res := make([]byte, len(plaintext))
+  for i := 0; i < len(plaintext) - (keysize-1); i += keysize {
+    block.Encrypt(res[i:i+keysize], plaintext[i:i+keysize])
+  }
+  return res
+}
+
+func decryptAesCbc(key []byte, ciphertext []byte, iv []byte) []byte{
+  blocksize := len(key)
+  if len(ciphertext)%blocksize != 0 {
+		panic("decryptAesEcb: cipher length is not multiple of keysize")
+	}
+
+  result := []byte("")
+  for i := 0; i < len(ciphertext); i += blocksize{
+    current_block := ciphertext[i:i+blocksize]
+    current_plain := fixedXOR(decryptAesEcb(key, current_block), iv)
+    result = append(result, current_plain...)
+    iv = current_block
+  }
+  return result
+}
+
+func encryptAesCbc(key []byte, plaintext []byte, iv []byte) []byte{
+  blocksize := len(key)
+  plaintext = addPkcsPadding(plaintext, blocksize)
+
+  result := []byte("")
+  for i := 0; i < len(plaintext); i += blocksize{
+    current_block := plaintext[i:i+blocksize]
+    current_cipher := encryptAesEcb(key, fixedXOR(current_block, iv))
+    result = append(result, current_cipher...)
+    iv = current_cipher
+  }
+  return result
 }
--- set2_test.go
+++ set2_test.go
@@ -4,11 +4,49 @@
 package main
 
 import "testing"
+import "fmt"
 
+// challenge 10
+import "bytes"
+import "io/ioutil"
+import "encoding/base64"
 
 func TestChallenge9(t *testing.T) {
   res := addPkcsPadding([]byte("YELLOW SUBMARINE"), 20)
   if string(res) != "YELLOW SUBMARINE\x04\x04\x04\x04" {
     t.Error("c9: wrong result", res)
+  }
+}
+
+func TestEncryptAesEcb(t *testing.T) {
+  key := []byte("YELLOW SUBMARINE")
+  plaintext := []byte("I'm back and I'm ringin' the bel")
+  res := encryptAesEcb(key, plaintext)
+  result := decryptAesEcb(key, res)
+  if string(result) != string(plaintext) {
+    t.Error("c10: wrong result:", string(result))
+  }
+}
+
+func TestChallenge10(t *testing.T) {
+  key := []byte("YELLOW SUBMARINE")
+  iv := bytes.Repeat([]byte("\x00"), 16)
+
+  in, err := ioutil.ReadFile("./10.txt")
+  if (err != nil) {
+    fmt.Println(err)
+  }
+
+  ciphertext, err := base64.StdEncoding.DecodeString(string(in))
+  if (err != nil) {
+    fmt.Println(err)
+  }
+
+  plaintext := decryptAesCbc(key, ciphertext, iv)
+  result := encryptAesCbc(key, plaintext, iv)
+
+
+  if string(result) != string(ciphertext) {
+    t.Error("c10: wrong result")
   }
 }
